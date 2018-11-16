f35f335f52a6402aca494166607f4f54e7666838
cpi <cpi@clockworkpi>
2018-11-16 11:37:48 +0000

    update gsp-installer.sh

--- gsp-installer.sh
+++ gsp-installer.sh
@@ -9,6 +9,8 @@ echo $'\n******* 2) Updating GSP, any local changes will be lost!'
 git fetch --all
 git reset --hard origin/master
 echo $'\n******* 3) Creating shortcuts'
+mkdir /home/cpi/apps/Menu/GameShell/GSPLauncher/
+
 echo /home/cpi/apps/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/games/GSP --use-gl=egl --ignore-gpu-blacklist --disable-accelerated-2d-canvas --num-raster-threads=2 --remote-debugging-port=9222 > /home/cpi/apps/Menu/GameShell/GSPLauncher/GSPLauncher.sh
 chmod +x /home/cpi/apps/Menu/GameShell/GSPLauncher/GSPLauncher.sh
 cp ./common/GSPLauncher.png /home/cpi/apps/Menu/GameShell/GSPLauncher/GSPLauncher.png
