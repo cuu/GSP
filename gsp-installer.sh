8a580d8bd3a0ebeb4be58e879a66650e73370b09
cpi <cpi@clockworkpi>
2018-11-16 11:33:53 +0000

    update to fit in launcher 2.0

--- gsp-installer.sh
+++ gsp-installer.sh
@@ -9,12 +9,12 @@ echo $'\n******* 2) Updating GSP, any local changes will be lost!'
 git fetch --all
 git reset --hard origin/master
 echo $'\n******* 3) Creating shortcuts'
-echo /home/cpi/GSP/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/GSP --use-gl=egl --ignore-gpu-blacklist --disable-accelerated-2d-canvas --num-raster-threads=2 --remote-debugging-port=9222 > /home/cpi/apps/launcher/Menu/GameShell/GSPLauncher.sh
-chmod +x /home/cpi/apps/launcher/Menu/GameShell/GSPLauncher.sh
-cp ./common/GSPLauncher.png /home/cpi/apps/launcher/skin/default/Menu/GameShell/GSPLauncher.png
+echo /home/cpi/apps/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/games/GSP --use-gl=egl --ignore-gpu-blacklist --disable-accelerated-2d-canvas --num-raster-threads=2 --remote-debugging-port=9222 > /home/cpi/apps/Menu/GameShell/GSPLauncher/GSPLauncher.sh
+chmod +x /home/cpi/apps/Menu/GameShell/GSPLauncher/GSPLauncher.sh
+cp ./common/GSPLauncher.png /home/cpi/apps/Menu/GameShell/GSPLauncher/GSPLauncher.png
 echo $'\n******* 4) removing archives...'
-rm *.tar.gz
-echo "***********************************************************************"
-echo "* If no errors, reboot your GameShell and select the GSPLauncher icon *" 
-echo "*                      HAVE FUN!                                      *"
-echo "***********************************************************************"
+#rm *.tar.gz
+echo "***********************************************************************************"
+echo "* If no errors, reload your GameShell Launcher UI and select the GSPLauncher icon *" 
+echo "*                                HAVE FUN!                                        *"
+echo "***********************************************************************************"
