8429e0ea18f8c90ceed3b108cf0bfbc9de27c8ca
cuu <dexterkidd@gmail.com>
2019-01-19 17:03:54 +0800

    fix instaler and updater scripts

--- gsp-installer.sh
+++ gsp-installer.sh
@@ -2,18 +2,21 @@
 echo $'\n******* 1) Installing NWjs...'
 wget https://github.com/LeonardLaszlo/nw.js-armv7-binaries/releases/download/v0.27.6/nwjs-sdk-v0.27.6-linux-arm.tar.gz
 tar -xvf nwjs-sdk-v0.27.6-linux-arm.tar.gz
-sudo cp nwjs-sdk-v0.27.6-linux-arm/lib/*.so /usr/lib
+
+#sudo cp nwjs-sdk-v0.27.6-linux-arm/lib/*.so /usr/lib
+
 sudo apt-get update
 sudo apt-get install libnss3-dev
 echo $'\n******* 2) Updating GSP, any local changes will be lost!'
 git fetch --all
 git reset --hard origin/master
 echo $'\n******* 3) Creating shortcuts'
-mkdir -p /home/cpi/apps/Menu/GameShell/GSPLauncher/
+mkdir -p /home/cpi/apps/Menu/21_Indie\ Games/GSPLauncher/
 
-echo /home/cpi/apps/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/games/GSP --use-gl=egl --ignore-gpu-blacklist --disable-accelerated-2d-canvas --num-raster-threads=2 --remote-debugging-port=9222 > /home/cpi/apps/Menu/GameShell/GSPLauncher/GSPLauncher.sh
-chmod +x /home/cpi/apps/Menu/GameShell/GSPLauncher/GSPLauncher.sh
-cp ./common/GSPLauncher.png /home/cpi/apps/Menu/GameShell/GSPLauncher/GSPLauncher.png
+echo LD_LIBRARY_PATH=/home/cpi/apps/nwjs-sdk-v0.27.6-linux-arm/lib /home/cpi/apps/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/games/GSP  --use-gl=egl --ignore-gpu-blacklist --disable-accelerated-2d-canvas --num-raster-threads=2 --remote-debugging-port=9222 > /home/cpi/apps/Menu/21_Indie\ Games/GSPLauncher/GSPLauncher.sh
+chmod +x /home/cpi/apps/Menu/21_Indie\ Games/GSPLauncher/GSPLauncher.sh
+cp ./common/GSPLauncher.png /home/cpi/apps/Menu/21_Indie\ Games/GSPLauncher/GSPLauncher.png
+
 echo $'\n******* 4) removing archives...'
 #rm *.tar.gz
 echo "***********************************************************************************"
--- gsp-updater.sh
+++ gsp-updater.sh
@@ -4,6 +4,7 @@ git fetch --all
 git reset --hard origin/master
 
 echo $'\n******* Re-reating shortcuts'
-echo /home/cpi/GSP/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/GSP --use-gl=egl --ignore-gpu-blacklist --disable-accelerated-2d-canvas --num-raster-threads=2 --remote-debugging-port=9222 > /home/cpi/apps/launcher/Menu/GameShell/GSPLauncher.sh
-chmod +x /home/cpi/apps/launcher/Menu/GameShell/GSPLauncher.sh
-cp ./common/GSPLauncher.png /home/cpi/apps/launcher/skin/default/Menu/GameShell/GSPLauncher.png
+echo LD_LIBRARY_PATH=/home/cpi/apps/nwjs-sdk-v0.27.6-linux-arm/lib /home/cpi/apps/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/games/GSP  --use-gl=egl --ignore-gpu-blacklist --disable-accelerated-2d-canvas --num-raster-threads=2 --remote-debugging-port=9222 > /home/cpi/apps/Menu/21_Indie\ Games/GSPLauncher/GSPLauncher.sh
+
+chmod +x /home/cpi/apps/Menu/21_Indie\ Games/GSPLauncher/GSPLauncher.sh
+cp ./common/GSPLauncher.png /home/cpi/apps/Menu/21_Indie\ Games/GSPLauncher/
